76d9190ee37e2fc813401df3db8642a5b87f7df6
Sangmin Lee <sangmlee23@gmail.com>
2025-09-18 12:06:24 -0400

    redirect www to non-www

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'header', key: 'host', value: '^www\\.smlee\\.dev$' }],
+        destination: 'https://smlee.dev/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
